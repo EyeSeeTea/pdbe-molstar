705b175ad5967817acdf1b728438e1b80218749e
Arnau Sanchez <pyarnau@gmail.com>
2021-05-25 10:51:13 +0200

    Add volume button to floating toolbar

--- src/app/ui/pdbe-viewport-controls.tsx
+++ src/app/ui/pdbe-viewport-controls.tsx
@@ -5,9 +5,34 @@ import { ToggleSelectionModeButton } from 'Molstar/mol-plugin-ui/structure/selec
 import { DownloadScreenshotControls } from 'Molstar/mol-plugin-ui/viewport/screenshot';
 import { SimpleSettingsControl } from 'Molstar/mol-plugin-ui/viewport/simple-settings';
 import { ViewportControls } from 'Molstar/mol-plugin-ui/viewport';
-import { AutorenewSvg, CameraOutlinedSvg, BuildOutlinedSvg, FullscreenSvg, TuneSvg, CloseSvg } from 'Molstar/mol-plugin-ui/controls/icons';
+import { AutorenewSvg, CameraOutlinedSvg, BuildOutlinedSvg, FullscreenSvg, TuneSvg, CloseSvg, BlurOnSvg } from 'Molstar/mol-plugin-ui/controls/icons';
+import { PluginUIComponent } from 'molstar/lib/mol-plugin-ui/base';
+import { VolumeStreamingControls, VolumeSourceControls } from 'molstar/lib/mol-plugin-ui/structure/volume';
+
+/* We need to add stateful components to PDBeViewportControls, but we cannot extend ViewportControls
+   with new props/state as ViewportControls is not generic. As a workaround, use a custom extended
+   state and perform casting when necessary. */
+
+type ViewportControlsState = ViewportControls extends PluginUIComponent<unknown, infer State> ? State : never;
+
+interface ControlsExtendedState extends ViewportControlsState {
+    isVolumeExpanded: boolean;
+}
+
+type ExtendedSetState = (cb: (prevState: ControlsExtendedState) => Partial<ControlsExtendedState>) => void;
 
 export class PDBeViewportControls extends ViewportControls {
+    constructor(props: any, context: any) {
+        super(props, context);
+        this.state = { ...this.state, isVolumeExpanded: false } as ViewportControlsState;
+    }
+
+    toggleVolumeExpanded = () => {
+        (this.setState as ExtendedSetState)(prevState => {
+            return { isVolumeExpanded: !prevState.isVolumeExpanded };
+        })
+    }
+
     isBlack(customeState: any): boolean{
         if(customeState && customeState.initParams && customeState.initParams.bgColor){
             const color = customeState.initParams.bgColor;
@@ -17,6 +42,7 @@ export class PDBeViewportControls extends ViewportControls {
     }
 
     render() {
+        const isVolumeExpanded = (this.state as ControlsExtendedState).isVolumeExpanded;
         const customeState: any = this.plugin.customState;
         let showPDBeLink = false;
         if(customeState && customeState.initParams && customeState.initParams.moleculeId && customeState.initParams.pdbeLink) showPDBeLink = true;
@@ -70,17 +96,32 @@ export class PDBeViewportControls extends ViewportControls {
                             <ToggleSelectionModeButton />
                         </div>
                     </div>}
+
+                    <div className="msp-pdbe-control-volume">
+                        <div className='msp-semi-transparent-background' />
+                        {this.icon(BlurOnSvg, this.toggleVolumeExpanded, 'Volume', isVolumeExpanded)}
+                    </div>
                 </div>
+
                 {this.state.isScreenshotExpanded && <div className='msp-viewport-controls-panel'>
                     <ControlGroup header='Screenshot / State' title='Click to close.' initialExpanded={true} hideExpander={true} hideOffset={true} onHeaderClick={this.toggleScreenshotExpanded}
                         topRightIcon={CloseSvg} noTopMargin childrenClassName='msp-viewport-controls-panel-controls'>
                         <DownloadScreenshotControls close={this.toggleScreenshotExpanded} />
                     </ControlGroup>
                 </div>}
+
                 {this.state.isSettingsExpanded && <div className='msp-viewport-controls-panel'>
                     <ControlGroup header='Settings / Controls Info' title='Click to close.' initialExpanded={true} hideExpander={true} hideOffset={true} onHeaderClick={this.toggleSettingsExpanded}
                         topRightIcon={CloseSvg} noTopMargin childrenClassName='msp-viewport-controls-panel-controls'>
                         <SimpleSettingsControl />
+                    </ControlGroup>
+                </div>}
+
+                {isVolumeExpanded && <div className='msp-viewport-controls-panel'>
+                    <ControlGroup header='Volume' title='Click to close.' initialExpanded={true} hideExpander={true} hideOffset={true} onHeaderClick={this.toggleVolumeExpanded}
+                        topRightIcon={CloseSvg} noTopMargin childrenClassName='msp-viewport-controls-panel-controls'>
+                        <VolumeStreamingControls />
+                        <VolumeSourceControls />
                     </ControlGroup>
                 </div>}
             </div>
