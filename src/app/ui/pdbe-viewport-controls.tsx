68a0de3aab640b5c2ae372340fa436f2ddc3964f
p3rcypj <pablo@percypj.com>
2023-08-23 17:51:57 +0000

    Revert "Remove pdbelink"
    This reverts commit e7254e0d9e1febad28c94171172ca82f5931bd6c.

--- src/app/ui/pdbe-viewport-controls.tsx
+++ src/app/ui/pdbe-viewport-controls.tsx
@@ -69,8 +69,22 @@ export class PDBeViewportControls extends ViewportControls {
         const isVolumeExpanded = (this.state as ControlsExtendedState)
             .isVolumeExpanded;
         const customeState: any = this.plugin.customState;
+        let showPDBeLink = false;
         let showControlToggle = true;
         let showControlInfo = true;
+        if (
+            customeState &&
+            customeState.initParams &&
+            customeState.initParams.moleculeId &&
+            customeState.initParams.pdbeLink
+        )
+            showPDBeLink = true;
+        if (
+            customeState &&
+            customeState.initParams &&
+            customeState.initParams.superposition
+        )
+            showPDBeLink = false;
         if (
             customeState &&
             customeState.initParams &&
@@ -88,143 +102,208 @@ export class PDBeViewportControls extends ViewportControls {
                 -1
         )
             showControlInfo = false;
+        const bgColor = this.isBlack(customeState) ? "#fff" : "#555";
+        const pdbeLink: any = {
+            parentStyle: { width: "auto" },
+            bgStyle: {
+                position: "absolute",
+                height: "27px",
+                width: "54px",
+                marginLeft: "-33px",
+            },
+            containerStyle: {
+                position: "absolute",
+                right: "10px",
+                top: "10px",
+                padding: "3px 3px 3px 18px",
+            },
+            style: {
+                display: "inline-block",
+                fontSize: "14px",
+                color: bgColor,
+                borderBottom: "none",
+                cursor: "pointer",
+                textDecoration: "none",
+                position: "absolute",
+                right: "5px",
+            },
+            pdbeImg: {
+                src: "https://www.ebi.ac.uk/pdbe/entry/static/images/logos/PDBe/logo_T_64.png",
+                alt: "PDBe logo",
+                style: {
+                    height: "12px",
+                    width: "12px",
+                    border: 0,
+                    position: "absolute",
+                    margin: "4px 0 0 -13px",
+                },
+            },
+        };
+        let vwpBtnsTopMargin = { marginTop: "30px" };
 
         return (
-            <div
-                className="msp-viewport-controls"
-                onMouseMove={this.onMouseMove}
-            >
-                <div className="msp-viewport-controls-buttons">
-                    <div className="msp-pdbe-control-reset-camera">
-                        <div className="msp-semi-transparent-background" />
-                        {this.icon(
-                            AutorenewSvg,
-                            this.resetCamera,
-                            "Reset Camera"
-                        )}
+            <>
+                {showPDBeLink && (
+                    <div
+                        className="msp-viewport-controls-buttons"
+                        style={pdbeLink.containerStyle}
+                    >
+                        <div
+                            className="msp-semi-transparent-background"
+                            style={pdbeLink.bgStyle}
+                        />
+                        <a
+                            className="msp-pdbe-link"
+                            style={pdbeLink.style}
+                            target="_blank"
+                            href={`https://pdbe.org/${customeState.initParams.moleculeId}`}
+                        >
+                            <img
+                                src={pdbeLink.pdbeImg.src}
+                                alt={pdbeLink.pdbeImg.alt}
+                                style={pdbeLink.pdbeImg.style}
+                            />
+                            {customeState.initParams.moleculeId}
+                        </a>
                     </div>
-                    <div className="msp-pdbe-control-snapshot">
-                        <div className="msp-semi-transparent-background" />
-                        {this.icon(
-                            CameraOutlinedSvg,
-                            this.toggleScreenshotExpanded,
-                            "Screenshot / State Snapshot",
-                            this.state.isScreenshotExpanded
-                        )}
-                    </div>
-                    <div>
-                        <div className="msp-semi-transparent-background" />
-                        <div className="msp-pdbe-control-toggle-controls-panel">
-                            {showControlToggle &&
-                                this.icon(
-                                    BuildOutlinedSvg,
-                                    this.toggleControls,
-                                    "Toggle Controls Panel",
-                                    this.plugin.layout.state.showControls
-                                )}
+                )}
+                <div
+                    className={"msp-viewport-controls"}
+                    onMouseMove={this.onMouseMove}
+                    style={showPDBeLink ? vwpBtnsTopMargin : void 0}
+                >
+                    <div className="msp-viewport-controls-buttons">
+                        <div className="msp-pdbe-control-reset-camera">
+                            <div className="msp-semi-transparent-background" />
+                            {this.icon(
+                                AutorenewSvg,
+                                this.resetCamera,
+                                "Reset Camera"
+                            )}
+                        </div>
+                        <div className="msp-pdbe-control-snapshot">
+                            <div className="msp-semi-transparent-background" />
+                            {this.icon(
+                                CameraOutlinedSvg,
+                                this.toggleScreenshotExpanded,
+                                "Screenshot / State Snapshot",
+                                this.state.isScreenshotExpanded
+                            )}
+                        </div>
+                        <div>
+                            <div className="msp-semi-transparent-background" />
+                            <div className="msp-pdbe-control-toggle-controls-panel">
+                                {showControlToggle &&
+                                    this.icon(
+                                        BuildOutlinedSvg,
+                                        this.toggleControls,
+                                        "Toggle Controls Panel",
+                                        this.plugin.layout.state.showControls
+                                    )}
+                            </div>
+                            {this.plugin.config.get(
+                                PluginConfig.Viewport.ShowExpand
+                            ) && (
+                                <div className="msp-pdbe-control-toggle-expanded-viewport">
+                                    {this.icon(
+                                        FullscreenSvg,
+                                        this.toggleExpanded,
+                                        "Toggle Expanded Viewport",
+                                        this.plugin.layout.state.isExpanded
+                                    )}
+                                </div>
+                            )}
+
+                            <div className="msp-pdbe-control-settings">
+                                {showControlInfo &&
+                                    this.icon(
+                                        TuneSvg,
+                                        this.toggleSettingsExpanded,
+                                        "Settings / Controls Info",
+                                        this.state.isSettingsExpanded
+                                    )}
+                            </div>
                         </div>
                         {this.plugin.config.get(
-                            PluginConfig.Viewport.ShowExpand
+                            PluginConfig.Viewport.ShowSelectionMode
                         ) && (
-                            <div className="msp-pdbe-control-toggle-expanded-viewport">
-                                {this.icon(
-                                    FullscreenSvg,
-                                    this.toggleExpanded,
-                                    "Toggle Expanded Viewport",
-                                    this.plugin.layout.state.isExpanded
-                                )}
+                            <div>
+                                <div className="msp-semi-transparent-background" />
+                                <div className="msp-pdbe-control-toggle-selection-mode">
+                                    <ToggleSelectionModeButton />
+                                </div>
                             </div>
                         )}
 
-                        <div className="msp-pdbe-control-settings">
-                            {showControlInfo &&
-                                this.icon(
-                                    TuneSvg,
-                                    this.toggleSettingsExpanded,
-                                    "Settings / Controls Info",
-                                    this.state.isSettingsExpanded
-                                )}
+                        <div className="msp-pdbe-control-volume">
+                            <div className="msp-semi-transparent-background" />
+                            {this.icon(
+                                BlurOnSvg,
+                                this.toggleVolumeExpanded,
+                                "Volume",
+                                isVolumeExpanded
+                            )}
                         </div>
                     </div>
-                    {this.plugin.config.get(
-                        PluginConfig.Viewport.ShowSelectionMode
-                    ) && (
-                        <div>
-                            <div className="msp-semi-transparent-background" />
-                            <div className="msp-pdbe-control-toggle-selection-mode">
-                                <ToggleSelectionModeButton />
-                            </div>
+
+                    {this.state.isScreenshotExpanded && (
+                        <div className="msp-viewport-controls-panel">
+                            <ControlGroup
+                                header="Screenshot / State"
+                                title="Click to close."
+                                initialExpanded={true}
+                                hideExpander={true}
+                                hideOffset={true}
+                                onHeaderClick={this.toggleScreenshotExpanded}
+                                topRightIcon={CloseSvg}
+                                noTopMargin
+                                childrenClassName="msp-viewport-controls-panel-controls"
+                            >
+                                <DownloadScreenshotControls
+                                    close={this.toggleScreenshotExpanded}
+                                />
+                            </ControlGroup>
                         </div>
                     )}
 
-                    <div className="msp-pdbe-control-volume">
-                        <div className="msp-semi-transparent-background" />
-                        {this.icon(
-                            BlurOnSvg,
-                            this.toggleVolumeExpanded,
-                            "Volume",
-                            isVolumeExpanded
-                        )}
-                    </div>
+                    {this.state.isSettingsExpanded && (
+                        <div className="msp-viewport-controls-panel">
+                            <ControlGroup
+                                header="Settings / Controls Info"
+                                title="Click to close."
+                                initialExpanded={true}
+                                hideExpander={true}
+                                hideOffset={true}
+                                onHeaderClick={this.toggleSettingsExpanded}
+                                topRightIcon={CloseSvg}
+                                noTopMargin
+                                childrenClassName="msp-viewport-controls-panel-controls"
+                            >
+                                <SimpleSettingsControl />
+                            </ControlGroup>
+                        </div>
+                    )}
+
+                    {isVolumeExpanded && (
+                        <div className="msp-viewport-controls-panel">
+                            <ControlGroup
+                                header="Volume"
+                                title="Click to close."
+                                initialExpanded={true}
+                                hideExpander={true}
+                                hideOffset={true}
+                                onHeaderClick={this.toggleVolumeExpanded}
+                                topRightIcon={CloseSvg}
+                                noTopMargin
+                                childrenClassName="msp-viewport-controls-panel-controls"
+                            >
+                                <VolumeStreamingControls />
+                                <VolumeSourceControls />
+                            </ControlGroup>
+                        </div>
+                    )}
                 </div>
-
-                {this.state.isScreenshotExpanded && (
-                    <div className="msp-viewport-controls-panel">
-                        <ControlGroup
-                            header="Screenshot / State"
-                            title="Click to close."
-                            initialExpanded={true}
-                            hideExpander={true}
-                            hideOffset={true}
-                            onHeaderClick={this.toggleScreenshotExpanded}
-                            topRightIcon={CloseSvg}
-                            noTopMargin
-                            childrenClassName="msp-viewport-controls-panel-controls"
-                        >
-                            <DownloadScreenshotControls
-                                close={this.toggleScreenshotExpanded}
-                            />
-                        </ControlGroup>
-                    </div>
-                )}
-
-                {this.state.isSettingsExpanded && (
-                    <div className="msp-viewport-controls-panel">
-                        <ControlGroup
-                            header="Settings / Controls Info"
-                            title="Click to close."
-                            initialExpanded={true}
-                            hideExpander={true}
-                            hideOffset={true}
-                            onHeaderClick={this.toggleSettingsExpanded}
-                            topRightIcon={CloseSvg}
-                            noTopMargin
-                            childrenClassName="msp-viewport-controls-panel-controls"
-                        >
-                            <SimpleSettingsControl />
-                        </ControlGroup>
-                    </div>
-                )}
-
-                {isVolumeExpanded && (
-                    <div className="msp-viewport-controls-panel">
-                        <ControlGroup
-                            header="Volume"
-                            title="Click to close."
-                            initialExpanded={true}
-                            hideExpander={true}
-                            hideOffset={true}
-                            onHeaderClick={this.toggleVolumeExpanded}
-                            topRightIcon={CloseSvg}
-                            noTopMargin
-                            childrenClassName="msp-viewport-controls-panel-controls"
-                        >
-                            <VolumeStreamingControls />
-                            <VolumeSourceControls />
-                        </ControlGroup>
-                    </div>
-                )}
-            </div>
+            </>
         );
     }
 }
